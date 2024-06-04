abd888a66bd4c35b3a705b43dd4a7237e92c0119
FabioBritto <fabio.tritono@gmail.com>
2024-06-04 16:19:14 -0300

    Commit project atividade3.1

---
+++ src/Variables01.java
@@ -0,0 +1,16 @@
+public class Variables01 {
+
+    public static void main(String[] args) {
+        String x = "Thiago";
+        System.out.println(" Navarro " +x);
+        System.out.println(x +" basquete e tao legal!");
+        System.out.println("ola " +x +"!");
+        System.out.println("por favor entre " +x +" meu Deus "
+                + "dia! meu nome e Navarro " +x +". eu sei como foi empolgante "
+                + "todos comecaram a apreciar " +x +" ligado " +x
+                + " dia apreciativo! " +x +", " +x +", " +x +"! verdade "
+                + x +"!!! esse sou eu! " +x +" foi o melhor encontro de todos!");
+        
+    }
+    
+}
